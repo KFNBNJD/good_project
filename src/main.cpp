1fbb53314fe6cd4e2c24920984861d677bbcba43
gdhsnlvr <gdhsnlvr@yandex.ru>
2015-08-17 15:39:56 +0300

    sfml

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 #include "EventSystem.h"
 #include "ObjectMovementHandler.h"
 
+#include "SFML/Graphics.hpp"
+
 int main() {
     ObjectFactory::get().registerClass("Object", new MovableObjectCreator());
 
@@ -18,5 +20,24 @@ int main() {
     objectMovementHandler.setObjects(ObjectFactory::get().getObjectMap());
     EventSystem::get().setHandler(Event::Type::ObjectMove, &objectMovementHandler);
     EventSystem::get().handle_events();
+
+    ////////////////////////////////////////
+    /// Creating window
+    ////////////////////////////////////////
+
+    sf::RenderWindow window(sf::VideoMode(800, 500), "Window title");
+
+    while (window.isOpen()) {
+    	sf::Event event;
+    	while (window.pollEvent(event)) {
+    		if (event.type == sf::Event::Closed) {
+    			window.close();
+    		}
+    	}
+
+    	window.clear();
+    	window.display();
+    }
+
     return 0;
 }
